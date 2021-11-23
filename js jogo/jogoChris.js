252325f2ddb0dc6cd5089afe0901e5090026bb6f
Nádia Mizael <nadia.mizael@gmail.com>
2021-11-23 11:05:10 -0300

    adiciona projeto github

---
+++ js jogo/jogoChris.js
@@ -0,0 +1,22 @@
+
+function escolhaFase1Chris(){
+    var escolha = prompt("Escolha o número da opção desejada");
+    if (escolha == 1 ) {
+        location = "../Chris/fase2Chris.html"
+        
+    }else {
+        location = "../Chris/gameoverChris1.html"
+    }
+}
+
+
+
+function escolhaFase2Chris(){
+    var escolha = prompt("Escolha o número da opção desejada");
+    if (escolha == 2 ) {
+        location = "../Chris/fase3Chris.html"
+        
+    }else {
+        location = "../Chris/gameoverChris2.html"
+    }
+}
